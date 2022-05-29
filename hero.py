90bcd635c7dc6e37be1848193456a6bb694bceaa
Noelle516 <noelle.rivera.516@gmail.com>
2022-05-28 20:25:53 -0500

    Added hero class

---
+++ hero.py
@@ -0,0 +1,84 @@
+import random as r
+
+class Hero:
+
+    def __init__(self, name, chClass, spellSlots,  sneakAttacks, damage, spellDamage, sneakDamage, boost):
+        self.name = name
+        self.chClass = chClass
+        self.spellSlots = spellSlots
+        self.sneakAttacks = sneakAttacks
+        self.damage  = damage
+        self.spellDamage = spellDamage
+        self.sneakDamage = sneakDamage
+        self.boost = boost
+        self.attackTypes = ["stick", "spell", "sneak"]
+
+    #getters for each field
+    def getName(self):
+        return self.name
+
+    def getchClass(self):
+        return self.chClass
+
+    def getSpellSlots(self):
+        return self.spellSlots
+
+    def getSneakAttacks(self):
+        return self.sneakAttacks
+    
+    def getDamage(self):
+        return self.damage
+
+    def getSpellDamage(self):
+        return self.spellDamage
+    
+    def getSneakDamage(self):
+        return self.sneakDamage
+
+    def getBoost(self):
+        return self.boost
+    
+    def getAttackTypes(self):
+        return self.attackTypes
+
+    #setters for method
+    def setName(self, n):
+        self.name = n
+
+    def setchClass(self, c):
+        self.chClass = c
+
+    def setSpellSlots(self, s):
+        self.spellSlots = s
+
+    def setSneakAttacks(self, s):
+        self.sneakAttacks
+    
+    def setDamage(self, s):
+        self.damage = s
+
+    def setSpellDamage(self, s):
+        self.spellDamage = s
+    
+    def setSneakDamage(self, s):
+        self.sneakDamage = s
+
+    def setBoost(self, s):
+        self.boost = s
+
+    def attack(self):
+        attackInput = input(f"Choose type of attack: {self.attackTypes} ")
+        enemyDamaged = 0
+        if attackInput == self.attackTypes[0]:
+            enemyDamaged = r.randint(0, self.damage) + self.boost
+            print(f"You used {self.attackTypes[0]}. It does {enemyDamaged} damage.")
+        elif attackInput == self.attackTypes[1]:
+            enemyDamaged = r.randint(0, self.spellDamage)
+            print(f"You used {self.attackTypes[1]}. It does {enemyDamaged} damage.")
+        elif attackInput == self.attackTypes[2]:
+            enemyDamaged = r.randint(0, self.sneakDamage)
+            print(f"You used {self.attackTypes[2]}. It does {enemyDamaged} damage.")
+        return enemyDamaged
+
+    def run():
+        return r.randint(1,3) == 3
--- main.py
+++ main.py
@@ -1,40 +1,17 @@
 import random as r
 import time
+from hero import  Hero
 
-characterName = "Gen"
-characterClass = "Player"
 availableClasses = ["strategic", "courageous", "sneaky"] # known as a list or an array
-spellSlot = 3
-sneakAttacks = 3
-damage = 3
-spellDamage = 5
-sneakDamage = 6
 moves = ["attack", "run", "use item"]
-attackTypes = ["stick", "spell", "sneak"]
 enemyHP = 4
-boost =  0
 bag = []
 
+player = Hero("Gen", "Player", 3, 3, 3, 5, 6, 0)
+
 def print_ascii_art(fn):
-    f = open(fn, 'r')
+    f = open(fn, 
     print( ''.join([line for line in f]))
-
-def attack():
-    attackInput = input(f"Choose type of attack: {attackTypes} ")
-    enemyDamaged = 0
-    if attackInput == attackTypes[0]:
-        enemyDamaged = r.randint(0, damage) + boost
-        print(f"You used {attackTypes[0]}. It does {enemyDamaged} damage.")
-    elif attackInput == attackTypes[1]:
-        enemyDamaged = r.randint(0, spellDamage)
-        print(f"You used {attackTypes[1]}. It does {enemyDamaged} damage.")
-    elif attackInput == attackTypes[2]:
-        enemyDamaged = r.randint(0, sneakDamage)
-        print(f"You used {attackTypes[2]}. It does {enemyDamaged} damage.")
-    return enemyDamaged
-
-def run():
-    return r.randint(1,3) == 3
 
 def useItem():
     if len(bag) == 0:
@@ -51,10 +28,10 @@ def encounter(eHP):
             hInput = input(f"Choose an action: {moves} ")
         print(f"You have chosen to {heroInput}")
         if hInput == "attack":
-            enemyDamaged = attack()
+            enemyDamaged = player.attack()
             eHP = eHP - enemyDamaged
         if hInput  == "run":
-            if run():
+            if player.run():
                 print("Successfully ran away!")
                 break
             else:
@@ -64,25 +41,26 @@ def encounter(eHP):
         hInput = ""
         print(f"The enemy has {eHP} hp.")
 
-characterName = input("Hello adventurer! What is your name? ").strip()
-print(f"Hello {characterName}!")
+player.setName(input("Hello adventurer! What is your name? ").strip())
+print(f"Hello {player.getName()}!")
 time.sleep(3)
 
-while (characterClass in availableClasses) == False:
+while (player.getchClass() in availableClasses) == False:
     characterClass = input("What kind of hero are you? [Strategic, Courageous, Sneaky] ").lower().strip()
+    player.setchClass(characterClass)
 
-print(f"You are a {characterClass} hero! That means you have the following attributes:")
-if characterClass == "strategic":
-    spellSlot = 5
-    damage = 2
+print(f"You are a {player.getchClass()} hero! That means you have the following attributes:")
+if player.getchClass() == "strategic":
+    player.setSpellSlots(5)
+    player.setDamage(2)
     print("You have bonus to spells, but do less physical damage")
-elif characterClass == "courageous":
-    damage = 5
-    sneakAttacks = 2
+elif player.getchClass() == "courageous":
+    player.setDamage(5)
+    player.setSneakAttacks(2)
     print("You have a bonus to damage, but have less sneak attacks")
 else:
-    sneakAttacks = 5
-    spellSlots = 2
+    player.setSneakAttacks(5)
+    player.setSpellSlots(2)
     print("You have bonus to sneak attacks, but have less spell slots")
 
 time.sleep(3)
@@ -102,7 +80,7 @@ while enemyHP > 0:
         heroInput = input(f"Choose an action: {moves} ")
     print(f"You have chosen to {heroInput}")
     if heroInput == "attack":
-        enemyDamaged = attack()
+        enemyDamaged = player.attack()
         enemyHP = enemyHP - enemyDamaged
     if heroInput  == "run":
         print("No running for tutorial quest")
@@ -112,7 +90,7 @@ while enemyHP > 0:
     print(f"The enemy has {enemyHP} hp.")
 
 print("Congrats! Enemy defeated. You have received a rusty blade (plus one to damage), 5 apples, a stick (to yeet at enemy)")
-boost = 1
+player.setBoost(1)
 bag.append("stick")
 for x in range(5):
     bag.append("apple")
